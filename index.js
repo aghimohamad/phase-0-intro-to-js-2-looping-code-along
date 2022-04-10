fbb3561c6b5cc02e0cc3666cc631e6fa67b46275
mohamad <aghi.mohamad@gmail.com>
2022-04-11 01:07:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(arr, eve) {
+    let narr = [];
+    for (let i=0; i<arr.length; i++) {
+        narr.push("Thank you, "+ arr[i] +", for the wonderful "+ eve +" gift!")
+    }    
+    return narr ;
+}
+
+console.log(writeCards(['gg', 'ff', 'ee'], "birthday"))
+
+
+
+function countDown(num){
+    let i = num; 
+    while(i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+countDown(10)
